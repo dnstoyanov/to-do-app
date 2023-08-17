7c5d28ca18b00aefa13a61289aae14960ddd71d8
dstoyanov <dobromir.nikolov.s@gmail.com>
2023-08-17 11:02:38 +0300

    removing interface that is not needed

--- src/components/SingleTask/SingleTask.tsx
+++ src/components/SingleTask/SingleTask.tsx
@@ -18,56 +18,46 @@ import {
 } from "../../api/api";
 import { useTasks } from "../../contexts/TaskContext";
 
-interface TaskProps {
-  task: Task;
-}
-const SingleTask: React.FC<TaskProps> = ({
+const SingleTask: React.FC<{ task: Task }> = ({
   task: { id, title, description, editedAt, isCompleted, isInProgress },
 }) => {
   const { deleteTask, updateTask } = useTasks();
   const formattedDate = moment(editedAt).format("D MMMM, YYYY h:mm:ss A z");
+  if (!id) return null;
 
   const handleDelete = async (id: number) => {
-    if (id !== undefined) {
-      try {
-        await deleteTaskByID(id);
-        deleteTask(id);
-      } catch (error) {
-        console.error("An error occurred while deleting the task:", error);
-      }
+    try {
+      await deleteTaskByID(id);
+      deleteTask(id);
+    } catch (error) {
+      console.error("An error occurred while deleting the task:", error);
     }
   };
 
   const handleComplete = async (id: number) => {
-    if (id !== undefined) {
-      try {
-        const updatedTask = await updateTaskCompletionStatusByID(id, true);
-        updateTask(updatedTask);
-      } catch (error) {
-        console.error("Error updating task:", error);
-      }
+    try {
+      const updatedTask = await updateTaskCompletionStatusByID(id, true);
+      updateTask(updatedTask);
+    } catch (error) {
+      console.error("Error updating task:", error);
     }
   };
 
   const handleProgress = async (id: number) => {
-    if (id !== undefined) {
-      try {
-        const updatedTask = await updateTaskInProgressStatusByID(id, true);
-        updateTask(updatedTask);
-      } catch (error) {
-        console.error("Error updating task:", error);
-      }
+    try {
+      const updatedTask = await updateTaskInProgressStatusByID(id, true);
+      updateTask(updatedTask);
+    } catch (error) {
+      console.error("Error updating task:", error);
     }
   };
 
   const handleToDo = async (id: number) => {
-    if (id !== undefined) {
-      try {
-        const updatedTask = await updateTaskToDoStatusByID(id, false);
-        updateTask(updatedTask);
-      } catch (error) {
-        console.error("Error updating task:", error);
-      }
+    try {
+      const updatedTask = await updateTaskToDoStatusByID(id, false);
+      updateTask(updatedTask);
+    } catch (error) {
+      console.error("Error updating task:", error);
     }
   };
 
@@ -86,7 +76,7 @@ const SingleTask: React.FC<TaskProps> = ({
           className={!isInProgress ? styles.disabled : styles.toDoBtn}
           data-tooltip-id="todo-btn"
           data-tooltip-content="Move task to To Do"
-          onClick={() => id !== undefined && handleToDo(id)}
+          onClick={() => handleToDo(id)}
         />
         <TbProgressCheck
           className={
@@ -94,10 +84,10 @@ const SingleTask: React.FC<TaskProps> = ({
           }
           data-tooltip-id="progress-btn"
           data-tooltip-content="Move task to In Progress"
-          onClick={() => id !== undefined && handleProgress(id)}
+          onClick={() => handleProgress(id)}
         />
         <IoMdDoneAll
-          onClick={() => id !== undefined && handleComplete(id)}
+          onClick={() => handleComplete(id)}
           data-tooltip-id="complete-btn"
           data-tooltip-content="Move task to Complete"
           className={isCompleted ? styles.disabled : styles.completeBtn}
@@ -107,7 +97,7 @@ const SingleTask: React.FC<TaskProps> = ({
           data-tooltip-id="delete-btn"
           data-tooltip-content="Delete task"
           className={styles.deleteBtn}
-          onClick={() => id !== undefined && handleDelete(id)}
+          onClick={() => handleDelete(id)}
         />
         <Tooltip id="progress-btn" />
         <Tooltip id="complete-btn" />
